ef48ad5157936ec43829a8a8d7297fab40fc067f
Olau Pla Richart <olau.pla.richart@gmail.com>
2022-07-30 14:29:51 +0200

    Added flash message

--- main/views.py
+++ main/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from .forms import ContactForm
+from django.contrib import messages
 
 def index(request):
     if request.POST:
         f = ContactForm(request.POST)
         if f.is_valid():
+            messages.success(request, "Message sent successfuly!")
             return redirect('home')
+            
             
 
     else:
