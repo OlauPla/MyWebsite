55e7e4f3856676860f21086d350cb009287ecf7d
Olau Pla Richart <olau.pla.richart@gmail.com>
2022-08-01 18:20:15 +0200

    Maintenence page

--- main/views.py
+++ main/views.py
@@ -3,21 +3,28 @@ from .forms import ContactForm
 from django.contrib import messages
 from .models import Contact
 
+MANTENIMIENTO = False #maintenence mode
 def index(request):
-    if request.POST:
-        f = ContactForm(request.POST)
-        if f.is_valid():
-            messages.success(request, "Message sent successfuly!")
-            name = f.cleaned_data["name"]
-            email = f.cleaned_data["email"]
-            message = f.cleaned_data["message"]
-            contact = Contact(name=name,email=email,message=message)
-            contact.save()
-            return redirect('home')
-            
-            
+
+    if MANTENIMIENTO == True:
+        return render(request, 'main/maintenence.html')
 
     else:
-        f = ContactForm()
+        if request.POST:
+            f = ContactForm(request.POST)
+            if f.is_valid():
+                messages.success(request, "Message sent successfuly!")
+                name = f.cleaned_data["name"]
+                email = f.cleaned_data["email"]
+                message = f.cleaned_data["message"]
+                contact = Contact(name=name,email=email,message=message)
+                contact.save()
+                return redirect('home')
+                
+                
 
-    return render(request, 'main/index.html', { 'form': f })
+        else:
+            f = ContactForm()
+
+        return render(request, 'main/index.html', { 'form': f })
+
