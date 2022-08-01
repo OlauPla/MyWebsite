685945e1e1d8d00ceb9d7493dc867fdbae291351
Olau Pla Richart <olau.pla.richart@gmail.com>
2022-08-01 14:01:45 +0200

    Added contact models for admin site

--- main/forms.py
+++ main/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=200,widget=forms.TextInput(attrs={'placeholder': 'Name'}),label="")
     email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email'}),label="")
-    message = forms.CharField(max_length=500, min_length=200,label="",widget=forms.Textarea(attrs={'placeholder': 'Message'}))
+    message = forms.CharField(max_length=500, min_length=100,label="",widget=forms.Textarea(attrs={'placeholder': 'Message'}))
--- main/views.py
+++ main/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, redirect
 from .forms import ContactForm
 from django.contrib import messages
+from .models import Contact
 
 def index(request):
     if request.POST:
         f = ContactForm(request.POST)
         if f.is_valid():
             messages.success(request, "Message sent successfuly!")
+            name = f.cleaned_data["name"]
+            email = f.cleaned_data["email"]
+            message = f.cleaned_data["message"]
+            contact = Contact(name=name,email=email,message=message)
+            contact.save()
             return redirect('home')
             
             
